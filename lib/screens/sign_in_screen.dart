ee00780cc77729cce4030464fea5dc40deac45b7
Irfan Andika <126872993+Irfan0535@users.noreply.github.com>
2024-04-08 09:52:07 +0700

    first commit

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum/screens/home_screen.dart';
+import 'package:fasum/screens/sign_up_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+  ],
+  clientId:
+      '410302709481-dj10c7h49tkmv1s63m9m1h7obuje4tbd.apps.googleusercontent.com',
+);
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key});
+  @override
+  SignInScreenState createState() => SignInScreenState();
+}
+
+class SignInScreenState extends State<SignInScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String _errorMessage = '';
+
+  Future<void> _signInWIthGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        await FirebaseAuth.instance.signInWithCredential(credential);
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      }
+    } catch (error) {
+      setState(() {
+        _errorMessage = error.toString();
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(_errorMessage),
+      ));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 32.0),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                    );
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  } catch (error) {
+                    setState(() {
+                      _errorMessage = error.toString();
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(_errorMessage),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Sign In'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton.icon(
+                  onPressed: _signInWIthGoogle,
+                  icon: Image.asset(
+                    'assets/images/icongoogle.png',
+                    height: 20,
+                  ),
+                  label: const Text(
+                    'Sign In with Google',
+                  )),
+              const SizedBox(height: 32.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SignUpScreen()),
+                  );
+                },
+                child: const Text('Don\'t have an account? Sign up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
